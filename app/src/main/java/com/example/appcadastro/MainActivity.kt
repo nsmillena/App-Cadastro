964be2c67e67a1bfe637117dad74d545626c4195
nsmillena <millenav.nunes08@gmail.com>
2025-04-09 18:06:12 -0300

    .'

---
+++ app/src/main/java/com/example/appcadastro/MainActivity.kt
@@ -0,0 +1,113 @@
+package com.example.appcadastro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.appcadastro.ui.theme.AppCadastroTheme
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.appcadastro.ui.theme.Darkblue
+import com.example.appcadastro.ui.theme.Lightblue
+import com.example.appcadastro.ui.theme.Pink40
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppCadastroTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                  appPreview()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProdutoItem() {
+    var nome by remember { mutableStateOf("") }
+    var telefone by remember { mutableStateOf("") }
+    var curso by remember { mutableStateOf("") }
+    var serie by remember { mutableStateOf("") }
+    Column(
+        Modifier
+            .verticalScroll(rememberScrollState())
+            .height(250.dp)
+            .width(200.dp)
+    ){
+        Box(
+            modifier = Modifier
+                .height(180.dp)
+                .background(
+                    brush = Brush.horizontalGradient(
+                        colors = listOf(
+                            Darkblue, Lightblue
+                        )
+                    )
+                )
+                .fillMaxWidth()
+        )
+        {
+            Image(
+                painter = painterResource(R.drawable.avatr_woman),
+                contentDescription = "avatar image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .offset(y = 70.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppCadastroTheme {
+        Greeting("Android")
+    }
+}
+
+
